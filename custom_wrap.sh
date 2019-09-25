d51e65503ad9820dda7d28374f8a3e278c713ff2
Danny van der Meulen <dmeulen@ebay.com>
2019-09-25 16:23:58 +0200

    Now really add the changes

--- custom_wrap.sh
+++ custom_wrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+env
+
+export CONSUL_ES_SERVICE
+
 coproc /usr/local/bin/consul-template \
   -consul-addr "consul.service.consul:8500" \
   -template "/unicast_hosts.tpl:/usr/share/elasticsearch/config/unicast_hosts.txt:true"
